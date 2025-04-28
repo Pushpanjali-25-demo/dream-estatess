ddc280eeccdc25b00cced958cb3ab1c46e93fee7
Pushpanjali-25-demo <pushpanjalihansdah123@gmail.com>
2025-04-29 02:07:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const searchButton = document.querySelector("button");
+    searchButton.addEventListener("click", function() {
+        alert("Searching properties...");
+    });
+});
