13b9edef392979232dd5d454dc72928cf710d15d
Pushpanjali-25-demo <pushpanjalihansdah123@gmail.com>
2025-04-29 01:34:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-
 // Responsive Menu
 const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
+
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('show');
 });
