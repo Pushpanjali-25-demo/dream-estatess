55610ee3967bcfb4b4fea609898547a93cfe2cf5
Pushpanjali-25-demo <pushpanjalihansdah123@gmail.com>
2025-05-01 03:46:25 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-// script.js
-document.addEventListener("DOMContentLoaded", function() {
-    const searchButton = document.querySelector("button");
-    searchButton.addEventListener("click", function() {
-        alert("Searching properties...");
-    });
-});
